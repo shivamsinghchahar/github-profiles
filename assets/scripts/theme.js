e1d51017456dcc17e4f7da4735d6c8504c9b93a5
chaharshivam <chahar.shivam@yahoo.com>
2019-08-02 23:29:39 +0530

    Update theme.js

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -4,7 +4,7 @@ const LOGO = document.querySelector('.logo');
 const FORM = document.querySelector('form');
 const INFO = document.querySelector('.info ul');
 
-let dark = JSON.parse(localStorage.getItem('dark')) || true;
+let dark = JSON.parse(localStorage.getItem('dark'));
 
 function defaultTheme() {
     if(dark) {
@@ -19,26 +19,17 @@ function defaultTheme() {
         INFO.style.color = '#0D0A0B';
     }
 };
-defaultTheme();
+
 
 function updateLocal (value) {
-    dark = !dark;
     localStorage.setItem('dark', `${value}`);
 }
 
 function switchTheme () {
+    dark = !dark;
     updateLocal(dark);
-    if(dark) {
-        THEME.innerHTML = '<small>&#x1f315;</small>';
-        document.body.style.background = '#051014';
-        LOGO.style.color = 'white';
-        INFO.style.color = 'white';
-    } else {
-        THEME.innerHTML = '<i class="fas fa-sun"></i>';
-        document.body.style.background = '#CDDDDD';
-        LOGO.style.color = '#051014';
-        INFO.style.color = '#0D0A0B';
-    }
+    defaultTheme();
 };
 
 THEME_BUTTON.addEventListener('click', switchTheme);
+defaultTheme();
